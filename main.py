c83a8fd52e77ce747a279b980fc8e32b43e73fd2
pimpale <gpimpale29@gmail.com>
2019-01-17 21:41:26 -0800

    testing uv light

--- main.py
+++ main.py
@@ -4,11 +4,15 @@ import numpy
 
 cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FPS, 30);
+cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0);
+# cap.set(cv2.CAP_PROP_EXPOSURE, 0.5);
+# cap.set(cv2.CAP_PROP_GAIN, 0.5);
 
 while True:
     frame = cap.read()[1]
-
-    cv2.imshow('frame3', frame)
+    blur = cv2.medianBlur(frame, 21)
+    hsv_thresh = cv2.inRange(cv2.cvtColor(blur, cv2.COLOR_BGR2HSV), (0, 0, 180), (180, 60, 255))
+    cv2.imshow('frame3', hsv_thresh)
     
     if cv2.waitKey(1) & 0xFF == ord('q'):
             break
